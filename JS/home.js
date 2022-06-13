8e62cce714d01232973f9e905d7b1b789acfedc5
k12e <kalyanirathod2095@gmail.com>
2022-06-13 10:59:28 +0530

    Added: address book list from local storage

--- JS/home.js
+++ JS/home.js
@@ -1,59 +1,69 @@
-/*uc3 tabular format*/
 window.addEventListener('DOMContentLoaded', (event) => {
     createInnerHtml();
-});
+  });
 
 const createInnerHtml = () => {
 
 const headerHtml =
-    `<tr>
-    <th>Name</th>
-    <th>Address</th>
-    <th>City</th>
-    <th>State</th>
-    <th>ZipCode</th>
-    <th>PhoneNumber</th>
-    <th>Actions</th>
-    </tr>`;
+       `<th>Name</th>
+        <th>Address</th>
+        <th>city</th>
+        <th>State</th>
+        <th>zipcode</th>
+        <th>phoneNumber</th>
+        <th>Actions</th>`;
 
-const innerHtml = `${headerHtml}
-<tr>
-<td>Kalyani Rathod</td>
-<td>gandhi nagar</td>
-<td>nagpur</td>
-<td> Maharashtra</td>
-<td>403 221</td>
-<td>91 9563245286</td>
-<td>
-<img onclick="remove(this)" alt="delete"src="../Assets/delete-black-18dp.svg">
-<img onclick="update(this)" alt="edit" src="../Assets/create-black-18dp.svg"  >
-</td>
-</tr>
-<tr>
-<td>Swati Badole</td>
-<td>rah nagar</td>
-<td>solapur</td>
-<td> Maharastra</td>
-<td>231 557</td>
-<td>91 9564526252</td>
-<td>
-<img onclick="remove(this)" alt="delete"src="../Assets/delete-black-18dp.svg">
-<img onclick="update(this)" alt="edit" src="../Assets/create-black-18dp.svg"  >
-</td>
-<tr>
-    
-<td>Ayansh Jadhao</td>
-<td>bardi</td>
-<td>Nagpur</td>
-<td> Maharashtra</td>
-<td>212 452</td>
-<td>91 7769845206</td>
-<td>
-    <img onclick="remove(this)" alt="delete"src="../Assets/delete-black-18dp.svg">
-    <img onclick="update(this)" alt="edit" src="../Assets/create-black-18dp.svg"  >
-    </td>
-</tr>
+let innerHtml = `${headerHtml}`;
+let contactList = createContactJSON();
+
+for(const Contact of contactList){
+innerHtml = `${innerHtml}
+      <tr>
+       
+        <td>${Contact._name}</td>
+        <td>${Contact._Address}</td>
+        <td>${Contact._city}</td>
+        <td>${Contact._State}</td>
+        <td>${Contact._zipcode}</td>
+        <td>${Contact._phoneNumber}</td>
+        
+        <td>
+        <img src="../assets/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
+        <img src="../assets/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
         </td>
     </tr>`;
+}
 document.querySelector('#table-display').innerHTML = innerHtml;
 }
+
+const createContactJSON = () => {
+    let contactListLocal = [
+      {
+        _name: 'Kalyani Rathod',
+        _Address: 'pusad',
+        _city:'pune',
+        _State:'maharastra',
+        _zipcode:455252,
+        _phoneNumber:919595656545
+         },
+         {
+            _name: 'Nayan Rathod',
+            _Address: 'digras',
+            _city:'nagpur',
+            _State:'maharastra',
+            _zipcode:455254,
+            _phoneNumber:919564756545
+             },
+             {
+                _name: 'Pavan Rathod',
+                _Address: 'ShivajiPark',
+                _city:'jalna',
+                _State:'maharastra',
+                _zipcode:451234,
+                _phoneNumber:919875565654
+                 },
+        
+    ];
+    return contactListLocal;
+  }
+  
